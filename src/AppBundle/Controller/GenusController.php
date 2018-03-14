2b5778efa67a2dd967898fe8e78f788ed12140e6
Qing Ma <qing.ma.web@gmail.com>
2018-03-14 22:54:35 +0100

    Repositoy fetches records!

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -30,6 +30,18 @@ class GenusController extends Controller
     }
 
     /**
+     * @Route("/genus")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $genuses = $em->getRepository('AppBundle:Genus')
+            ->findAll();
+
+        dump($genuses);die;
+    }
+
+    /**
      * @Route("/genus/{genusName}")
      */
     public function showAction($genusName)

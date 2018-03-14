12d4188a3be3c1f17300ad9eca7efd76a1292ea1
Qing Ma <qing.ma.web@gmail.com>
2018-03-14 22:37:25 +0100

    entity manager adds new records to the genus table

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -20,4 +20,21 @@ class Genus
      * @ORM\Column(type="string")
      */
     private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
 }

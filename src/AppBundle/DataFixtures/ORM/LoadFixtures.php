a1b192448c42cde5e9c39a5ae1156192f171c827
Qing Ma <qing.ma.web@gmail.com>
2018-03-15 22:27:11 +0100

    Nelmio\Alice load fixtures from a yml file

--- src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -4,16 +4,12 @@ namespace AppBundle\DataFixtures\ORM;
 use AppBundle\Entity\Genus;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Fixtures;
 
 class LoadFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $genus = new Genus();
-        $genus->setName('Octopus' . rand(1, 100));
-        $genus->setSubFamily('Octopodinae');
-        $genus->setSpeciesCount(rand(100, 99999));
-        $manager->persist($genus);
-        $manager->flush();
+        Fixtures::load(__DIR__.'/fixtures.yml', $manager);
     }
 }

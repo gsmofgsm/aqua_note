516c2ec03062bf8a10335f622c5f01d158f82ecd
Qing Ma <qing.ma.web@gmail.com>
2018-03-16 22:22:37 +0100

    recent note count

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -80,8 +80,14 @@ class GenusController extends Controller
         }
         */
 
+        $recentNotes = $genus->getNotes()
+            ->filter(function(GenusNote $note) {
+               return $note->getCreatedAt() > new \DateTime('-3 months');
+            });
+
         return $this->render('genus/show.html.twig', [
-            'genus' => $genus
+            'genus' => $genus,
+            'recentNoteCount' => count($recentNotes)
         ]);
     }
 

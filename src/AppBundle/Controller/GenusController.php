423ce224a35ece3d12928b8b088db68314bd27f3
Qing Ma <qing.ma.web@gmail.com>
2018-03-16 22:50:41 +0100

    now use the real markdownparser in our transformer service using dependency injection

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -66,7 +66,7 @@ class GenusController extends Controller
             throw $this->createNotFoundException('No genus found');
         }
 
-        $transformer = new MarkdownTransformer();
+        $transformer = new MarkdownTransformer($this->get('markdown.parser'));
         $funFact = $transformer->parse($genus->getFunFact());
 
         /*

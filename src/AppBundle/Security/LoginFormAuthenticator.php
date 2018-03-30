e6ee18a7c3a8e4b99f25a1ebd0452eb1085b523b
Qing Ma <qing.ma.web@gmail.com>
2018-03-30 22:37:11 +0200

    login form is handled in Authenticator. Step 1: if right url and post method, return credentials

---
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Security;
+
+use AppBundle\Form\LoginForm;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+{
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
+    public function __construct(FormFactoryInterface $formFactory)
+    {
+
+        $this->formFactory = $formFactory;
+    }
+
+    public function getCredentials(Request $request)
+    {
+        $isLoginSubmit = $request->attributes->get('_route') === 'security_login'
+            && $request->isMethod('POST');
+
+        if (!$isLoginSubmit) {
+            return;
+        }
+
+        $form = $this->formFactory->create(LoginForm::class);
+        $form->handleRequest($request);
+        $data = $form->getData();
+
+        return $data;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        // TODO: Implement getUser() method.
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // TODO: Implement checkCredentials() method.
+    }
+
+    protected function getLoginUrl()
+    {
+        // TODO: Implement getLoginUrl() method.
+    }
+
+}

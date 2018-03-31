e07299f345657413bc3d6fc303d65aec7d3439d2
Qing Ma <qing.ma.web@gmail.com>
2018-03-31 11:21:22 +0200

    HashPasswordLister for before persisting and updating User

---
+++ src/AppBundle/Doctrine/HashPasswordListener.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\Doctrine;
+
+
+use AppBundle\Entity\User;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+
+class HashPasswordListener implements EventSubscriber
+{
+
+    protected $passwordEncoder;
+
+    public function __construct(UserPasswordEncoder $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * Returns an array of events this subscriber wants to listen to.
+     *
+     * @return array
+     */
+    public function getSubscribedEvents()
+    {
+        return ['prePersist', 'preUpdate'];
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if(!$entity instanceof User){
+            return;
+        }
+
+        $this->encodePassword($entity);
+    }
+
+    public function preUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if(!$entity instanceof User){
+            return;
+        }
+
+        $this->encodePassword($entity);
+    }
+
+    /**
+     * @param User $entity
+     */
+    protected function encodePassword(User $entity)
+    {
+        $encoded = $this->passwordEncoder->encodePassword($entity, $entity->getPlainPassword());
+        $entity->setPassword($encoded);
+    }
+}
